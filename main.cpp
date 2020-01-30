177c393456d538d3bdbcddc5698ec37285a651bb
pyrotank41 <pyrotank41@gmail.com>
2020-01-30 10:05:46 -0600

    Added test cases in main.cpp along with some comments in mymatrix.h

--- main.cpp
+++ main.cpp
@@ -3,51 +3,210 @@
 
 using namespace std;
 
-int access_test()
+bool access_test() // checking if at and operator () is working. 
 {
-  mymatrix<int> M;
+  cout << "Matrix element access test: ";
+  try
+  {
+    mymatrix<int> M;
+    int x = M.at(3,3);
+    x = M(3,2);
+    cout << "Pass" << endl;
+    return true;  // since it reached here, at and () access works.
+  }
+  catch(const std::exception& e)
+  {
+    cout << "Fail" << endl;
+    return false;
+  }
   
-  return M.at(3,3);
+  
 }
 
-void edit_test(mymatrix<int>& M, int newDefault = 1) // checking if the the elements of the vector is editing or not.
+bool edit_test() // checking if the the elements of the matrix is editing or not.
 {
-  int numRow = M.numrows();
-
-  for(int r =0; r < numRow; ++r)
+  cout << "Matrix element access and modify test: ";
+  try
   {
-    int numCol = M.numcols(r);
-    for(int c =0; c < numCol; ++c){
-      // cout << r << "," << c << "  "; 
-      M(r,c) = newDefault;
+    mymatrix<int> M;
+    int numRow = M.numrows();
+    int newDefault = 1;
+
+    for(int r =0; r < numRow; ++r)
+    {
+      int numCol = M.numcols(r);
+      for(int c =0; c < numCol; ++c){
+        M(r,c) = newDefault;
+      }
     }
-    // cout << endl;
+    cout << "Pass" << endl;
+    return true;
+  }
+  catch(const std::exception& e)
+  {
+    cout << "Fail" << endl;
+    return false;
+  }
+  
+}
+
+bool growcol_test() // testing the growth function.
+{
+  cout << "growcol() test: ";
+  try
+  {
+    mymatrix<int>  M;  // 4x4 matrix, initialized to 0
+ 
+    M(0, 0) = 123;
+    M(1, 1) = 456;
+    M(2, 2) = 789;
+    M(3, 3) = -99;
+
+    M.growcols(1, 8);  // increase # of cols in row 1 to 8
+    
+    cout << "Pass" << endl;
+    return true; // grow works well
+  }
+  catch(exception e)
+  {
+    cout << "Fail" << endl;
+    return false;
+  }
+  
+}
+
+bool grow_test()
+{
+  cout << "grow() test: ";
+  try
+  { 
+    mymatrix<int> M1(2,2);
+    M1(0,0) = 1;
+    M1(0,1) = 2;
+    M1(1,0) = 3;
+    M1(0,1) = 4;
+    M1.grow(5,5);
+    cout << "Pass" << endl;
+    return true; // grow works well
+  }
+  catch(exception e)
+  {
+    cout << "Fail" << endl;
+    return false;
+  }
+}
+
+bool scalar_test()
+{
+  cout << "scalar multiplication test: ";
+  try
+  {
+    mymatrix<int> M1;
+    
+    M1(0, 0) = 2;
+    M1(1, 1) = 2;
+    M1(2, 2) = 2;
+    M1(3, 3) = 2;
+    
+    M1 = M1*2;
+    
+    cout << "Pass" << endl << "\nOutput:";
+    M1._output();
+    return true; // grow works well
+  }
+  catch(exception e)
+  {
+    cout << "Fail" << endl;
+    return false;
+  }
+}
+
+void isEqual(mymatrix<int>& M, mymatrix<int>& MA)
+{
+  if(M.numrows() != MA.numrows()){
+    throw invalid_argument("isEqual:: Rows does not match");
+  }
+  
+  for (int r = 0; r < M.numrows(); r++)
+  {
+
+    if(M.numcols(r) != MA.numcols(r))
+    {
+      throw invalid_argument("isEqual:: colums does not match");
+    }
+    for (int c = 0; c < M.numcols(r); c++)
+    {
+      if(M(r,c) != MA(r,c))
+      {
+        throw invalid_argument("isEqual:: Matrix is not equal");
+      }
+    }
+    
+  }
+  
+}
+
+bool matmulti_test()
+{
+  cout << "matrix multiplication test: ";
+  try
+  {
+    mymatrix<int> M1(4,5);
+    mymatrix<int> M2(5,3);
+    
+    M1(0, 0) = 2;
+    M1(1, 1) = 2;
+    M1(2, 2) = 2;
+    M1(3, 3) = 2;
+
+    M2(0, 0) = 1;
+    M2(1, 1) = 1;
+    M2(2, 2) = 1;
+    M2(3, 2) = 1;
+
+    mymatrix<int> M3 = M1*M2;
+    mymatrix<int> MA(4,3);
+    M3._output();// fix me
+    MA._output();//fix me
+
+    isEqual(M3,MA);
+    cout << "Pass" << endl << "\nOutput:";
+    M3._output();
+    return true; // grow works well
+  }
+  catch(const std::exception& e)
+  {
+    cout << "Fail" << endl;
+    std::cerr << e.what() << '\n';
+    return false;
   }
 }
 
 
+void run_tests()
+{
+  int passed = 0;
+  int count = 6;
+
+
+  passed = passed + access_test();
+  passed = passed + edit_test();
+  passed = passed + growcol_test();
+  passed = passed + grow_test();
+  passed = passed + scalar_test();
+  passed = passed + matmulti_test();
+
+  cout << endl;
+  cout << "Total tests passed: " << passed << endl;
+  cout << "Total tests failed: " << count - passed << endl;
+
+}
+
 int main() {
   
-  cout << "Matrix1 is:" << endl; 
-  mymatrix<int> yo(3,3);
-  
-  yo.grow(3, 4);
-  edit_test(yo);
-  yo._output();
-  // yo._output();
-  
-  // cout << "Matrix size is " << yo.size() << endl;
+  run_tests();
 
-  cout << "Matrix2 is:" << endl; 
-  mymatrix<int> multi(3,3);
-  edit_test(multi, 3); 
-  multi.grow(4,3);
-  multi._output();
-  
-  cout << "Matrix2 * Matrix1 is:" << endl;
-  
-  mymatrix<int> multi1 = yo*multi;
-  // multi._output();
   return 0;
+  
  
 }
--- mymatrix.h
+++ mymatrix.h
@@ -1,45 +1,49 @@
 	
-/*mymatrix.h*/
+/*mymatrix.h*
  
-//
-// Karan Singh Kochar
-// U. of Illinois, Chicago
-// CS 251: Spring 2020
-// Project #02
-//
+/*
+  Karan Singh Kochar
+  U. of Illinois, Chicago
+  CS 251: Spring 2020
+  Project #02
+*/
  
-//
-// mymatrix
-//
-// The mymatrix class provides a matrix (2D array) abstraction.
-// The size can grow dynamically in both directions (rows and
-// cols).  Also, rows can be "jagged" --- i.e. rows can have
-// different column sizes, and thus the matrix is not necessarily
-// rectangular.  All elements are initialized to the default value
-// for the given type T.  Example:
-//
-//   mymatrix<int>  M;  // 4x4 matrix, initialized to 0
-//  
-//   M(0, 0) = 123;
-//   M(1, 1) = 456;
-//   M(2, 2) = 789;
-//   M(3, 3) = -99;
-//
-//   M.growcols(1, 8);  // increase # of cols in row 1 to 8
-//
-//   for (int r = 0; r < M.numrows(); ++r)
-//   {
-//      for (int c = 0; c < M.numcols(r); ++c)
-//         cout << M(r, c) << " ";
-//      cout << endl;
-//   }
-//
-// Output:
-//   123 0 0 0
-//   0 456 0 0 0 0 0 0
-//   0 0 789 0
-//   0 0 0 -99
-//
+/*
+  mymatrix
+
+  The mymatrix class provides a matrix (2D array) abstraction.
+  The size can grow dynamically in both directions (rows and
+  cols).  Also, rows can be "jagged" --- i.e. rows can have
+  different column sizes, and thus the matrix is not necessarily
+  rectangular.  All elements are initialized to the default value
+  for the given type T.  Example:
+
+*/
+
+/*    
+  // Sample use case.
+  mymatrix<int>  M;  // 4x4 matrix, initialized to 0
+ 
+  M(0, 0) = 123;
+  M(1, 1) = 456;
+  M(2, 2) = 789;
+  M(3, 3) = -99;
+
+  M.growcols(1, 8);  // increase # of cols in row 1 to 8
+
+  for (int r = 0; r < M.numrows(); ++r)
+  {
+     for (int c = 0; c < M.numcols(r); ++c)
+        cout << M(r, c) << " ";
+     cout << endl;
+  }
+
+  Output:
+    123 0 0 0
+    0 456 0 0 0 0 0 0
+    0 0 789 0
+    0 0 0 -99
+*/
  
 #pragma once
  
@@ -53,6 +57,7 @@ template<typename T>
 class mymatrix
 {
 private:
+  // Variables ------------------------------------------------------------------------
   struct ROW
   {
     T*  Cols;     // dynamic array of column elements
@@ -61,14 +66,53 @@ private:
  
   ROW* Rows;     // dynamic array of ROWs
   int  NumRows;  // total # of rows (0..NumRows-1)
+
+  // private functions -----------------------------------------------------------------
+  
+  // isRectangular
+  // Checking of a matrix is rectangular
+  bool isRectangular(const mymatrix<T>& other){
+    
+    int numCols = Rows[0].NumCols; // number of columns of 1st row.
+    for( int r = 1; r < NumRows; r++)
+    { 
+      if(numCols != Rows[r].NumCols ){ // checking #columns v/s row's #columns
+        return false; // false if there is a missmatch.
+      }
+    }
+    return true; // returns true if not missmatch.
+  }
+
+  // isMultipliable:
+  //
+  // Can we multiply two matrix?  Only if M1 is R1xN and M2 is NxC2. 
+  // This yields a result that is R1xC2.
+  // Here other is one matrix and "This" object is another.
+  // Example: 3x4 * 4x2 => 3x2
+  
+  bool isMultipliable(const mymatrix<T>& other){
+
+    if(Rows[0].NumCols == other.NumRows) //size RxN and NxC check.
+    { 
+      return true;
+    }
+    else 
+    {
+      return false;
+    }
+  } 
+
+  // private ends here -------------------------------------------------------------------
  
 public:
-  //
-  // default constructor:
+  
+  
+  
+  // default constructor: ----------------------------------------------------------------
   //
   // Called automatically by C++ to construct a 4x4 matrix.  All
   // elements are initialized to the default value of T.
-  //
+  
   mymatrix()
   {
     Rows = new ROW[4];  // an array with 4 ROW structs:
@@ -88,8 +132,7 @@ public:
     }
   }
  
-  //
-  // parameterized constructor:
+  // parameterized constructor: ------------------------------------------------------------
   //
   // Called automatically by C++ to construct a matrix with R rows,
   // where each row has C columns. All elements are initialized to
@@ -117,9 +160,7 @@ public:
     }
   }
  
- 
-  //
-  // copy constructor:
+  // copy constructor: -------------------------------------------------------------------------
   //
   // Called automatically by C++ to construct a matrix that contains a
   // copy of an existing matrix.  Example: this occurs when passing
@@ -130,14 +171,16 @@ public:
   //
   mymatrix(const mymatrix<T>& other)
   { 
-    this->Rows = new ROW[other.NumRows];
-    this->NumRows = other.NumRows;
+    this->Rows = new ROW[other.NumRows]; //Creating And array of ROW struct.
+    this->NumRows = other.NumRows;      
 
     for (int r = 0; r < NumRows; ++r)
     { 
       int NumCols = other.Rows[r].NumCols;
-      this->Rows[r].Cols = new T[NumCols];
+      this->Rows[r].Cols = new T[NumCols]; // creating new Cols array
       this->Rows[r].NumCols = NumCols;
+
+      //copying over all the elements from other matrix over to prevent shallow copy.
       for (int c = 0; c < other.NumRows; ++c)
       {
         this->Rows[r].Cols[c] = other.Rows[r].Cols[c];        
@@ -145,9 +188,7 @@ public:
     }
   }
  
- 
-  //
-  // numrows
+  // numrows: -----------------------------------------------------------------------------------
   //
   // Returns the # of rows in the matrix.  The indices for these rows
   // are 0..numrows-1.
@@ -158,35 +199,33 @@ public:
   }
    
  
-  //
-  // numcols
-  //
+
+  // numcols: ------------------------------------------------------------------------------------
+  
   // Returns the # of columns in row r.  The indices for these columns
   // are 0..numcols-1.  Note that the # of columns can be different
   // row-by-row since "jagged" rows are supported --- matrices are not
   // necessarily rectangular.
-  //
+
   int numcols(int r) const
-  {
+  { 
     if (r < 0 || r >= NumRows)
       throw invalid_argument("mymatrix::numcols: row");
 
     return Rows[r].NumCols;
   }
 
+  //  growcols: ----------------------------------------------------------------------------------
+    
+  //   Grows the # of columns in row r to at least C.  If row r contains
+  //   fewer than C columns, then columns are added; the existing elements
+  //   are retained and new locations are initialized to the default value
+  //   for T.  If row r has C or more columns, then all existing columns
+  //   are retained -- we never reduce the # of columns.
+    
+  //   Jagged rows are supported, i.e. different rows may have different
+  //   column capacities -- matrices are not necessarily rectangular.
 
-  //
-  // growcols
-  //
-  // Grows the # of columns in row r to at least C.  If row r contains
-  // fewer than C columns, then columns are added; the existing elements
-  // are retained and new locations are initialized to the default value
-  // for T.  If row r has C or more columns, then all existing columns
-  // are retained -- we never reduce the # of columns.
-  //
-  // Jagged rows are supported, i.e. different rows may have different
-  // column capacities -- matrices are not necessarily rectangular.
-  //
   void growcols(int r, int C)
   {
     if (r < 0 || r >= NumRows)
@@ -212,23 +251,22 @@ public:
     
   }
 
-  //
-  // grow
-  //
+  // grow : ----------------------------------------------------------------------------------------
+  
   // Grows the size of the matrix so that it contains at least R rows,
   // and every row contains at least C columns.
-  //
+  
   // If the matrix contains fewer than R rows, then rows are added
   // to the matrix; each new row will have C columns initialized to
   // the default value of T.  If R <= numrows(), then all existing
   // rows are retained -- we never reduce the # of rows.
-  //
+  
   // If any row contains fewer than C columns, then columns are added
   // to increase the # of columns to C; existing values are retained
   // and additional columns are initialized to the default value of T.
   // If C <= numcols(r) for any row r, then all existing columns are
   // retained -- we never reduce the # of columns.
-  //
+  
   void grow(int R, int C)
   {
     if (R < 1)
@@ -246,17 +284,17 @@ public:
   
       
       int r;
-      for( r = 0; r < R; ++r) // storing previous rows to the tempRows.
+      for( r = 0; r < R; ++r) 
       {
         tempRows[r].Cols = new T[C];
         tempRows[r].NumCols = C;
-
-        if(r < NumRows)
+        
+        if(r < NumRows) // storing previous rows to the tempRows.
         {
           for( int c = 0; c < C; ++c)
           tempRows[r].Cols[c] = at(r, c);
         }
-        else
+        else // Storing default values in rest of the remaining places.
         {
           for( int c = 0; c < C; ++c)
           tempRows[r].Cols[c] = T{};
@@ -264,8 +302,6 @@ public:
         
       } 
 
-      
-      
       delete[] Rows; // Deleting old rows for new Rows.
       this->Rows = tempRows;
       NumRows = R;
@@ -274,11 +310,11 @@ public:
   }
  
  
-  //
-  // size
-  //
+  
+  // size: -------------------------------------------------------------------------------------
+  // 
   // Returns the total # of elements in the matrix.
-  //
+  
   int size() const
   {
     int size = 0; 
@@ -290,28 +326,26 @@ public:
     return size;
   }
  
- 
-  //
-  // at
+  // at : ----------------------------------------------------------------------------------------
   //
   // Returns a reference to the element at location (r, c); this
   // allows you to access the element or change it:
-  //
-  //    M.at(r, c) = ...
-  //    cout << M.at(r, c) << endl;
-  //
+  //  
+  //   M.at(r, c) = ...
+  //   cout << M.at(r, c) << endl;
+
   T& at(int r, int c)
   {
     if (r < 0 || r >= NumRows)
     {
-      cout << "** error: trying to access rowIndex = " << r << " where maxRows = " << NumRows<< endl; 
+      //cout << "** error: trying to access rowIndex = " << r << " where maxRows = " << NumRows<< endl; 
       throw invalid_argument("mymatrix::at: row");
     }
       
       
     if (c < 0 || c >= Rows[r].NumCols)
     { 
-      cout << "** error: trying to access colIndex = " << c << " where maxCols = " << Rows[r].NumCols << endl; 
+      //cout << "** error: trying to access colIndex = " << c << " where maxCols = " << Rows[r].NumCols << endl; 
       throw invalid_argument("mymatrix::at: col");
     }
       
@@ -320,15 +354,14 @@ public:
   }
  
  
-  //
-  // ()
-  //
+  // () : --------------------------------------------------------------------------------------------
+  
   // Returns a reference to the element at location (r, c); this
   // allows you to access the element or change it:
-  //
-  //    M(r, c) = ...
-  //    cout << M(r, c) << endl;
-  //
+  
+  //   M(r, c) = ...
+  //   cout << M(r, c) << endl;
+
   T& operator()(int r, int c)
   {
     if (r < 0 || r >= NumRows)
@@ -338,16 +371,15 @@ public:
  
     return Rows[r].Cols[c];
   }
- 
-  //
-  // scalar multiplication
-  //
+
+  // scalar multiplication : ---------------------------------------------------------------------------
+  
   // Multiplies every element of this matrix by the given scalar value,
   // producing a new matrix that is returned.  "This" matrix is not
   // changed.
-  //
-  // Example:  M2 = M1 * 2;
-  //
+  
+  // Example:  M2 = M1 * 2;  
+
   mymatrix<T> operator*(T scalar)
   {
     mymatrix<T> result;
@@ -364,122 +396,72 @@ public:
     return result;
   }
 
-  //
-  // Checking rectangular matrix
-  //
- 
-  bool isRectangular(const mymatrix<T>& other){
-    
-    int numCols = Rows[0].NumCols; // number of columns of 1st row.
-    for( int r = 1; r < NumRows; r++)
-    { 
-      if(numCols != Rows[r].NumCols ){ // checking #columns v/s row's #columns
-        return false; // false if there is a missmatch.
-      }
-    }
-    return true; // returns true if not missmatch.
-  }
-
-
-  bool isMultipliable(const mymatrix<T>& other){
-
-    if(Rows[0].NumCols == other.NumRows) //size RxN and NxC check.
-    { 
-      return true;
-    }
-    else 
-    {
-      return false;
-    }
-  } 
-  //
-  // matrix multiplication
-  //
+  
+  
+  
+  // matrix multiplication: --------------------------------------------------------------------------------
+  
   // Performs matrix multiplication M1 * M2, where M1 is "this" matrix and
   // M2 is the "other" matrix.  This produces a new matrix, which is returned.
   // "This" matrix is not changed, and neither is the "other" matrix.
-  //
+  
   // Example:  M3 = M1 * M2;
-  //
+  
   // NOTE: M1 and M2 must be rectangular, if not an exception is thrown.  In
   // addition, the sizes of M1 and M2 must be compatible in the following sense:
   // M1 must be of size RxN and M2 must be of size NxC.  In this case, matrix
   // multiplication can be performed, and the resulting matrix is of size RxC.
-  //
+  
   mymatrix<T> operator*(const mymatrix<T>& other)
   {
     
-    int debug = 0;
-    mymatrix<T> result(1,1); // keeping result matrix to minimum.
-    mymatrix<T> matrix2(1,1);
-
-    matrix2.Rows = other.Rows;
-    matrix2.NumRows = other.NumRows;
-
-    // matrix2._output();
-
-    //
     // both matrices must be rectangular for this to work:
-    //
     if (!isRectangular(*this))
       throw runtime_error("mymatrix::*: this not rectangular");
     
     if (!isRectangular(other))
       throw runtime_error("mymatrix::*: other not rectangular");
  
-    //
-    // Okay, both matrices are rectangular.  Can we multiply?  Only
-    // if M1 is R1xN and M2 is NxC2.  This yields a result that is
-    // R1xC2.
-    //
-    // Example: 3x4 * 4x2 => 3x2
-    //
+   
 
     if (!isMultipliable(other))
       throw runtime_error("mymatrix::*: size mismatch");
   
-    else{
-      
-      int resRows = this->NumRows; // this will be our new NumRows.
-      int resCols = other.Rows[0].NumCols; // this will be our new NumCols.
-      
-      result.grow(resRows,resCols); // resizing output matrix.
-      //result._output();
+    mymatrix<T> result(1,1); // keeping result matrix to minimum.
+    mymatrix<T> matrix2(1,1); // "
 
-      for(int i = 0; i < resRows; ++i)
-      {
-        for(int j = 0; j < resCols; ++j)
-        { 
-          T sum = T{};
-          for(int k = 0; k < this->Rows[0].NumCols; ++k){
-            
-            T a = at(i,k);
-            T b = matrix2.at(k,j);
-            // T a = 0;
-            // T b = 0;
-            // debugger for matrix multiplication. 
-            if(debug!=0) cout << "|("<< i << "," << k << ")*" << "("<< k << "," << j << ")" << " = " << a <<"*" << b << "|   ";
+    // creating a shallow copy of other matrix to get arround Const.
+    matrix2.Rows = other.Rows;
+    matrix2.NumRows = other.NumRows;
 
-            sum = sum + (a * b);
-          }
-          if( debug != 0) cout << endl;
-          result(i,j) = sum;  
-        } 
+    int resRows = this->NumRows; // this will be our new NumRows.
+    int resCols = other.Rows[0].NumCols; // this will be our new NumCols.
+    
+    result.grow(resRows,resCols); // resizing output matrix.
 
-        if( debug !=0 ) cout <<endl;
-        
+
+    for(int i = 0; i < resRows; ++i)
+    {
+      for(int j = 0; j < resCols; ++j)
+      { 
+        T sum = T{};
+        for(int k = 0; k < this->Rows[0].NumCols; ++k){
+          
+          T a = at(i,k);
+          T b = matrix2.at(k,j);
+          sum = sum + (a * b);
+        }
+        result(i,j) = sum;  
       }
-      
     }
-    result._output();
+    
     return result;
   }
- 
-  //
-  // _output
+
+  // _output : ---------------------------------------------------------------------------
   //
   // Outputs the contents of the matrix; for debugging purposes.
-  //
+
   void _output()
   { 
     cout <<endl;
